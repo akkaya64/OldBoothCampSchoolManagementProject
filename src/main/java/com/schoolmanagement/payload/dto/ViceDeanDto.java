05cae087aa55fd54af39b30272b9fa5a4068bcda
Fatih Akkaya <akkaya064@gmail.com>
2023-06-03 19:26:16 +0100

    Day9_ViceDean_Creating_Controller_Service_Response_Request_DTO_Classes

---
+++ src/main/java/com/schoolmanagement/payload/dto/ViceDeanDto.java
@@ -0,0 +1,25 @@
+package com.schoolmanagement.payload.dto;
+
+import com.schoolmanagement.entity.concretes.ViceDean;
+import com.schoolmanagement.payload.request.ViceDeanRequest;
+import lombok.Data;
+
+@Data
+public class ViceDeanDto {
+
+    public ViceDean dtoViceBean(ViceDeanRequest viceDeanRequest){
+
+        return ViceDean.builder()
+                .birthDay(viceDeanRequest.getBirthDay())
+                .username(viceDeanRequest.getUsername())
+                .name(viceDeanRequest.getName())
+                .surname(viceDeanRequest.getSurname())
+                .password(viceDeanRequest.getPassword())
+                .ssn(viceDeanRequest.getSsn())
+                .birthPlace(viceDeanRequest.getBirthPlace())
+                .phoneNumber(viceDeanRequest.getPhoneNumber())
+                .gender(viceDeanRequest.getGender())
+                .build();
+
+    }
+}
---
+++ src/main/java/com/schoolmanagement/payload/response/ViceDeanResponse.java
@@ -0,0 +1,6 @@
+package com.schoolmanagement.payload.response;
+
+import com.schoolmanagement.payload.response.abstracts.BaseUserResponse;
+
+public class ViceDeanResponse extends BaseUserResponse {
+}
